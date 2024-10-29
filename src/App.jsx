0eb8a150e120af8f20f9a8e9b6c35f68ce1c1433
Mardonbek <163328721+mardonbekovich@users.noreply.github.com>
2024-10-29 18:58:07 +0500

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import Header from './components/Header/Header';
+import Hero from './components/Hero/Hero';
+import Catalog from './components/Catalog/Catalog';
+
+const App = () => {
+  return (
+    <>
+      <Header />
+      <Hero />
+      <Catalog/>
+    </>
+  );
+};
+
+export default App;
